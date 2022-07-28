2ec13db712a9bb07648c9b44c426ebec8ecee4fe
mirarsl <batuhanarslan@yasamedya.com>
2022-07-28 17:58:24 +0300

    connection fix

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '185.226.196.151'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'byfix_db'),
             'username' => env('DB_USERNAME', 'byfix_user'),
