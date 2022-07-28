26768a18cfdd0083861d87d694dedfa86d5df00b
mirarsl <batuhanarslan@yasamedya.com>
2022-07-28 18:48:27 +0300

    connection fix

--- app/Http/Controllers/InformationsController.php
+++ app/Http/Controllers/InformationsController.php
@@ -17,15 +17,19 @@ class InformationsController extends Controller
             $value = Cache::get('info');
             return $value;
         }
-
-        $information = Informations::select('site_baslik AS baslik','site_slogan AS slogan','site_desc AS aciklama','site_tel AS tel','site_whatsapp AS whatsapp','site_mail AS email','site_gsm AS gsm','adres_bilgisi AS adres')->get();
-        $response = response()->json([
-            'data' => $information[0],
-            'status'=>200,
-            'created_at' => date('Y-m-d h:i:s',time())
-        ],200);
-        Cache::put('info',$response, $seconds = 300);
-        return $response;
+        try {
+           $information = Informations::select('site_baslik AS baslik','site_slogan AS slogan','site_desc AS aciklama','site_tel AS tel','site_whatsapp AS whatsapp','site_mail AS email','site_gsm AS gsm','adres_bilgisi AS adres')->get();
+           $response = response()->json([
+                'data' => $information[0],
+                'status'=>200,
+                'created_at' => date('Y-m-d h:i:s',time())
+            ],200);
+           
+           Cache::put('info',$response, $seconds = 300);
+           return $response;   
+        } catch (Exception $e) {
+            echo $e;
+        }
     }
     
 }
--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'byfixstore.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'byfix_db'),
             'username' => env('DB_USERNAME', 'byfix_user'),
